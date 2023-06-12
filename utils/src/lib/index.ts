9bc88d61b48089c3387d35fad1728f8cb1b78a10
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2023-06-11 20:20:11 -0700

    .

--- utils/src/lib/index.ts
+++ utils/src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./chunk";
 export * from "./discord";
 export * from "./getfiles";
+export * from "./responders";
---
+++ utils/src/lib/responders.ts
@@ -0,0 +1,71 @@
+import {
+    MessagePayload,
+    MessageCreateOptions,
+    InteractionEditReplyOptions,
+    EmbedBuilder,
+    ChatInputCommandInteraction,
+    InteractionReplyOptions,
+    InteractionResponse,
+    InteractionDeferReplyOptions,
+    Message,
+} from "discord.js";
+
+type TextBasedChannelSendOption =
+    | string
+    | MessagePayload
+    | MessageCreateOptions;
+
+type CommonInteractionEditReplyOptions =
+    | string
+    | MessagePayload
+    | InteractionEditReplyOptions;
+
+export function embed(): EmbedBuilder {
+    return new EmbedBuilder().setColor(0xff0092).setTimestamp();
+}
+
+export function getInteractionResponder(interaction: ChatInputCommandInteraction, handleErrors = log) {
+    return {
+        reply: async (
+            options: InteractionReplyOptions
+        ): Promise<void | InteractionResponse> => {
+            return await interaction.reply(options).catch(handleErrors);
+        },
+        defer: async (
+            options?: InteractionDeferReplyOptions
+        ): Promise<void | InteractionResponse> => {
+            return await interaction.deferReply(options).catch(handleErrors);
+        },
+
+        edit: async (
+            options: CommonInteractionEditReplyOptions
+        ): Promise<void | Message> => {
+            return await interaction.editReply(options).catch(handleErrors);
+        },
+        send: async (
+            options: TextBasedChannelSendOption
+        ): Promise<void | Message> => {
+            if (!interaction.channel || !interaction.channel.isTextBased()) {
+                return;
+            }
+            return await interaction.channel.send(options).catch(handleErrors);
+        },
+    };
+}
+
+export type getInteractionResponders = {
+    send: (options: TextBasedChannelSendOption) => Promise<void | Message>;
+    edit: (
+        options: CommonInteractionEditReplyOptions
+    ) => Promise<void | Message>;
+    defer: (
+        options?: InteractionDeferReplyOptions
+    ) => Promise<void | InteractionResponse>;
+    reply: (
+        options: InteractionReplyOptions
+    ) => Promise<void | InteractionResponse>;
+};
+
+function log(e: unknown) {
+    console.warn(e);
+}
